9db62d63028204d1e58e7a517746c29e17fc0cd7
Cyril Jade Concoles <concolescyriljade@gmail.com>
2026-03-02 22:21:25 +0800

    [pages] Add initial Vue application routes

---
+++ pages/terms.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts"> 
+ 
+import { useSeoMeta, useHead } from '@vueuse/head'; 
+ 
+const title = "Obsidian Key | Home"; 
+const description = "Unlock Futures"; 
+ 
+useSeoMeta({ 
+  title: () => title, 
+  description: () => description, 
+  charset: "utf-8", 
+  viewport: "width=device-width, initial-scale=1.0" 
+}); 
+ 
+useHead({ 
+  link: [ 
+    {rel: 'icon', type: 'image/png', href: '/logo.png'}, 
+    {rel: 'stylesheet', href: '/reset.css'} 
+  ] 
+});</script>
+
+<template>   
+  <client-only> 
+    <NavBar /> 
+    <Welcome /> 
+    <Copyright /> 
+  </client-only> 
+</template>
+
+<style scoped></style> 
+ 
+<style></style> 
