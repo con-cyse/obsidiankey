036ac134c1bbe819b09d2d72026c036a4c94b028
Cyril Jade Concoles <concolescyriljade@gmail.com>
2026-03-15 16:40:00 +0800

    [feat] Edit policy.vue file.

--- pages/policy.vue
+++ pages/policy.vue
@@ -1,9 +1,20 @@
+<script lang="ts"></script> 
+ 
+<template> 
+    <client-only> 
+        <!-- <NavBar /> --> 
+        <div id="okMain" class="ok-main"></div> 
+        <!-- <Cookies /> --> 
+        <!-- <Copyright /> --> 
+    </client-only> 
+</template> 
+ 
 <script setup lang="ts"> 
  
-//import { useSeoMeta, useHead } from '@vueuse/head'; 
+import { useSeoMeta, useHead } from '@vueuse/head'; 
  
 const title = "Obsidian Key | Home"; 
-const description = "Unlock Futures"; 
+const description = "A personalized course matching tailored to your strengths and interests."; 
  
 useSeoMeta({ 
   title: () => title, 
@@ -15,18 +26,16 @@ useSeoMeta({
 useHead({ 
   link: [ 
     {rel: 'icon', type: 'image/png', href: '/logo.png'}, 
-    {rel: 'stylesheet', href: '/reset.css'} 
+    {rel: 'stylesheet', href: '/reset.css'}, 
+    {rel: 'stylesheet', href: '/custom.css'} , 
+    {rel: 'preconnect', href: 'https://fonts.googleapis.com'}, 
+    {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''},
+    {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300..700;1,300..700&family=Noto+Sans+Display:ital,wght@0,100..900;1,100..900&display=swap'}
+ 
   ] 
-});</script>
-
-<template>   
-  <client-only> 
-    <NavBar /> 
-    <Welcome /> 
-    <Copyright /> 
-  </client-only> 
-</template>
-
+});
+</script> 
+ 
 <style scoped></style> 
  
-<style></style> 
+<style></style>
