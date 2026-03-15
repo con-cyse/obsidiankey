36b90295a931c5fac1265f71d46a977705fbe46d
Cyril Jade Concoles <concolescyriljade@gmail.com>
2026-03-15 18:03:11 +0800

    [UI] Add NavBar component with design and usage

---
+++ components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>  
+  <client-only>  
+    <header class="ok-navbar">  
+      <nav class="ok-nav-left"> 
+          <a href="/" class="ok-nav-home"> 
+            <img src="/logo.png" width=20 height=20 class="ok-nav-logo"> 
+            <span>Obsidian Key</span> 
+          </a>  
+      </nav>  
+      <nav class="ok-nav-right">  
+        <ul> 
+          <li><a href="#" class="ok-nav-link">Generate Course Recommendations</a></li> 
+          <li><a href="#" class="ok-nav-link">View Course Details</a></li> 
+          <li><a href="#" class="ok-nav-link">Update Profile</a></li> 
+        </ul> 
+      </nav>  
+    </header>  
+  </client-only>  
+</template>
