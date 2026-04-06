553c30e3a110a124cac91e5e5858ca216295262f
CxxJay <23-1-02121@vsu.edu.ph>
2026-04-06 22:02:28 +0800

    [feat] Edit naming in NavBar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,31 +1,31 @@
 <template>  
   <client-only>  
-    <header :class="['bh-navbar', navAdminMode]">  
-      <nav class="bh-nav-left"> 
-          <a href="/" class="bh-nav-home"> 
-            <img src="/logo.png" width=20 height=20 class="bh-nav-logo"> 
-            <span>Bleedingheart</span> 
+    <header :class="['ok-navbar', navAdminMode]">   
+      <nav class="ok-nav-left"> 
+          <a href="/" class="ok-nav-home"> 
+            <img src="/logo.png" width=20 height=20 class="ok-nav-logo"> 
+            <span>Obsidian Key</span> 
           </a>  
       </nav>  
-      <nav id="bhNavBasic" class="bh-nav-right bh-hidden">  
+      <nav id="okNavBasic" class="ok-nav-right ok-hidden">  
         <ul> 
-          <li><a href="#" class="bh-nav-link">3R Guide</a></li> 
-          <li><a href="#" class="bh-nav-link">Leaderboard</a></li> 
-          <li><a href="#" class="bh-nav-link">Contribute</a></li> 
-          <li><span id="bhNavBasicLogout" class="bh-nav-link">Logout</span></li> 
+          <li><a href="#" class="ok-nav-link">Generate Course Recommendations</a></li> 
+          <li><a href="#" class="ok-nav-link">View Course Details</a></li> 
+          <li><a href="#" class="ok-nav-link">Update Profile</a></li>
+          <li><span id="okNavBasicLogout" class="ok-nav-link">Logout</span></li> 
         </ul> 
-      </nav> 
-      <nav id="bhNavAdmin" class="bh-nav-right bh-hidden">  
+      </nav>  
+      <nav id="okNavAdmin" class="ok-nav-right ok-hidden">  
         <ul> 
-          <li><a href="#" class="bh-nav-link">Settings</a></li> 
-          <li><a href="#" class="bh-nav-link">Reports</a></li> 
-          <li><span id="bhNavAdminLogout" class="bh-nav-link">Logout</span></li> 
+          <li><a href="#" class="ok-nav-link">Settings</a></li> 
+          <li><a href="#" class="ok-nav-link">Reports</a></li> 
+          <li><span id="okNavAdminLogout" class="ok-nav-link">Logout</span></li> 
         </ul> 
-      </nav> 
+      </nav>
     </header>  
   </client-only>  
-</template> 
- 
+</template>
+
 <script setup lang="ts"> 
 const props = defineProps<{ navAdminMode: string }>(); 
 </script>
